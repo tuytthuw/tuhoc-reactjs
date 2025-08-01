0a07585b46391f9e1cae159c59b8093a8c517ef9
Tuyết Thư <nguyenthituyetthu2k3@gmail.com>
2025-08-01 19:51:23 +0700

    chore: update project configuration and dependencies
    - Added Tailwind CSS and related plugins to package.json
    - Removed App.css and migrated styles to index.css using Tailwind
    - Updated App.tsx to reflect new styling and removed unused classes
    - Enhanced tsconfig.json with baseUrl and paths for easier imports
    - Configured Vite to use Tailwind CSS and added server settings
    - Introduced .editorconfig, .prettierignore, and .prettierrc for consistent code formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,41 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist", "vite.config.ts"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: { prettier: eslintPluginPrettier },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          semi: false,
+          trailingComma: "none",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true,
+        },
+      ],
+    },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
